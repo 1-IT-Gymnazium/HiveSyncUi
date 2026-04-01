5b098af77e5f8b2130c88a48520efc01bd852843
WestrCZ <janekhujer@gmail.com>
2026-04-01 11:41:45 +0200

    enhanced RoundTextField looks

--- app/frontend/src/components/common/input/RoundTextField.tsx
+++ app/frontend/src/components/common/input/RoundTextField.tsx
@@ -1,8 +1,11 @@
 import {
+  formLabelClasses,
   inputBaseClasses, outlinedInputClasses, styled, TextField,
 } from "@mui/material";
 
-const RoundTextField = styled(TextField)(({ theme }) => ({
+const RoundTextField = styled(TextField)(({
+  theme,
+}) => ({
   [`.${inputBaseClasses.root}`]: {
     backgroundColor: theme.palette.background.paper,
     borderRadius: "28px",
@@ -14,6 +17,22 @@ const RoundTextField = styled(TextField)(({ theme }) => ({
   },
   [`.${inputBaseClasses.sizeSmall}`]: {
     borderRadius: "20px",
+  },
+  [`.${formLabelClasses.root}`]: {
+    backgroundColor: theme.palette.primary.dark,
+    borderRadius: "20px",
+    color: theme.palette.primary.contrastText,
+    padding: "0.125rem 0.5rem",
+    [`&.${formLabelClasses.focused}`]: {
+      color: theme.palette.primary.contrastText,
+    },
+  },
+  "& input[type=\"color\"]": {
+    cursor: "pointer",
+    height: "66px",
+    margin: "-5px",
+    padding: 0,
+    width: "calc(100% + 8px)",
   },
 }));
 
